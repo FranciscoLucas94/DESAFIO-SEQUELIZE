37fa5dfd7cdcbd0595d4416dc23729effdfeb56a
Francsico Lucas <158366952+FranciscoLucas94@users.noreply.github.com>
2026-04-24 18:34:08 -0300

    Update orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -6,5 +6,8 @@ const c = new OrderController();
 
 router.post('/pedidos', (req, res) => c.create(req, res));
 router.get('/pedidos', (req, res) => c.getAll(req, res));
+router.get('/pedidos/:id', (req, res) => c.getById(req, res));
+router.put('/pedidos/:id', (req, res) => c.update(req, res));
+router.delete('/pedidos/:id', (req, res) => c.delete(req, res));
 
 export default router;
