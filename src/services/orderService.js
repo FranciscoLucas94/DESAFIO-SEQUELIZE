17cee0da006d9e18db946f1c3614986c2dcce717
Francsico Lucas <158366952+FranciscoLucas94@users.noreply.github.com>
2026-04-24 18:32:46 -0300

    Update orderService.js

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -23,3 +23,29 @@ export const createOrder = async ({ usuario_id, produto_id, quantidade }) => {
 export const getOrders = async () => {
   return await Order.findAll({ include: [User, Product] });
 };
+
+export const updateOrder = async (id, { usuario_id, produto_id, quantidade }) => {
+  const order = await Order.findByPk(id);
+  if (!order) throw new Error("Pedido não encontrado");
+
+  const user = await User.findByPk(usuario_id);
+  if (!user) throw new Error("Usuário não existe");
+
+  const product = await Product.findByPk(produto_id);
+  if (!product) throw new Error("Produto não existe");
+
+  if (quantidade > product.estoque) {
+    throw new Error("Estoque insuficiente");
+  }
+
+  product.estoque -= quantidade;
+  await product.save();
+
+  return await order.update({ usuario_id, produto_id, quantidade });
+};
+
+export const deleteOrder = async (id) => {
+  const order = await Order.findByPk(id);
+  if (!order) throw new Error("Pedido não encontrado");
+  return await order.destroy();
+};
