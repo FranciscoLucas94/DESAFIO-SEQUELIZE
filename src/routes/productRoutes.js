f85463471addf6bccac75b2a50f310d893107d91
Francsico Lucas <158366952+FranciscoLucas94@users.noreply.github.com>
2026-04-24 18:35:00 -0300

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -6,5 +6,8 @@ const c = new ProductController();
 
 router.post('/produtos', (req, res) => c.create(req, res));
 router.get('/produtos', (req, res) => c.getAll(req, res));
+router.get('/produtos/:id', (req, res) => c.getById(req, res));
+router.put('/produtos/:id', (req, res) => c.update(req, res));
+router.delete('/produtos/:id', (req, res) => c.delete(req, res));
 
 export default router;
