ed0cf6cc15b1fc91e0a23dd9c01306fae1db6042
Francsico Lucas <158366952+FranciscoLucas94@users.noreply.github.com>
2026-04-24 18:35:41 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,5 +6,8 @@ const c = new UserController();
 
 router.post('/usuarios', (req, res) => c.create(req, res));
 router.get('/usuarios', (req, res) => c.getAll(req, res));
+router.get('/usuarios/:id', (req, res) => c.getById(req, res));
+router.put('/usuarios/:id', (req, res) => c.update(req, res));
+router.delete('/usuarios/:id', (req, res) => c.delete(req, res));
 
 export default router;
